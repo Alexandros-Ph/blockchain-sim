ca1f951f28b34a903c2546d7e5efdcf5063add40
Alex <alex.pachos1@gmail.com>
2019-03-17 19:49:10 +0200

    Validate block & Hash functions.

--- block.py
+++ block.py
@@ -2,8 +2,9 @@ import hashlib
 from time import ti
 
 class Block(object):
-    # Initialize a block
-    def __init__(self, index, prev_hash):
+
+    def __init__(self, index, transactions, prev_hash):
+        # Initialize a block
         self.index = index
         self.timestamp = time()
         self.transactions = transactions
@@ -11,13 +12,15 @@ class Block(object):
         self.current_hash = str()
         self.previous_hash = prev_hash
 
-    @staticmethod
-    def hash(block):
-        # Hashes a Block
-        pass
+    def hash(self):
+        # Creates a SHA-256 hash of a Block
+        block_dict = del (self.__dict__)['current_hash']                    # object to dictionary conversion excluding current hash
+        block_string = json.dumps(block_dict, sort_keys=True).encode()      # dictionary to string conversion
+        return hashlib.sha256(block_string).hexdigest()                     # string to sha256 hash
 
-    def validate_block(block, prev_hash):
+
+    def validate_block(self, prev_hash):
         # Validates current and previous hash of Block
-        pass
-
-    
+        x = (self.previous_hash == prev_hash)
+        y = (self.current_hash == hash(self))
+        return (x and y)
