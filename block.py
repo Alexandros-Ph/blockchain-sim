9cc9befd7539d8aea081834393d96fcf9f5248aa
Alex <alex.pachos1@gmail.com>
2019-03-19 00:08:34 +0200

    Fix - Validate block & Hash functions tested.

--- block.py
+++ block.py
@@ -1,5 +1,6 @@
 import hashlib
-from time import ti
+import json
+from time import time
 
 class Block(object):
 
@@ -14,7 +15,8 @@ class Block(object):
 
     def hash(self):
         # Creates a SHA-256 hash of a Block
-        block_dict = del (self.__dict__)['current_hash']                    # object to dictionary conversion excluding current hash
+        block_dict = self.__dict__                                          # object to dictionary conversion
+        del block_dict['current_hash']                                      # delete current hash key from dictionary
         block_string = json.dumps(block_dict, sort_keys=True).encode()      # dictionary to string conversion
         return hashlib.sha256(block_string).hexdigest()                     # string to sha256 hash
 
@@ -22,5 +24,13 @@ class Block(object):
     def validate_block(self, prev_hash):
         # Validates current and previous hash of Block
         x = (self.previous_hash == prev_hash)
-        y = (self.current_hash == hash(self))
+        y = (self.current_hash == self.hash())
         return (x and y)
+
+'''
+temp = Block(1, [], 1995)
+print(vars(temp))
+temp.current_hash = temp.hash()
+print(vars(temp))
+print(temp.validate_block(1995))
+'''
