23674a136c93ea5a0e47f7ae3df61e46e3a70269
euaggelia-ktk <48314187+euaggelia-ktk@users.noreply.github.com>
2019-03-19 12:36:45 +0100

    Add files via upload
    dokimh 1 2

--- blockchain.py
+++ blockchain.py
@@ -1,18 +1,46 @@
+import hashlib
+import json
+from time import time
+from uuid import uuid4
+
 class Blockchain(object):
-    def __init__(self):
+    def __init__(self,difficulty):
+        self.difficulty = difficulty
         self.chain = []
         self.current_transactions = []
+        self.append_block(previous_hash=1, nonce=0)
 
-    def append_block(block, self):
+    def append_block(self, previous_hash, nonce):
         # Creates a new Block and adds it to the chain
-        pass
+        index = len(self.chain) + 1
+        transactions = self.current_transactions
+        block = Block(index,transactions,previous_hash)
+        
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+
+    def add_transaction(self, sender_address, receiver_address, amount):
+        self.current_transaction.append({
+            'sender': sender_address,
+            'recipient': receiver_address,
+            'amount': amount,
+        })
+        return self.last_block['index'] +1
+
+
 
     def validate_chain(self):
         # Calls validate block for every block of the validate_chain
-        pass
-
+        for i in range (1,len(blockchain)):
+            current_block = blockchain[i]
+            previous_block_hash = blockchain[i-1].hash
+            if (!validate_block(current_block,previous_block_hash)):
+                return False
+        reurn True
 
     @property
     def last_block(self):
-        # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
+
